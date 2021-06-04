28c590ef4f05a56250eb5febc88140134820a2c3
bendrendel <bfdrendel@gmail.com>
2021-06-04 13:32:04 -0600

    Pass search results state down

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -5,6 +5,32 @@ import { Playlist } from '../Playlist/Playlist.js'
 import './App.css';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchResults: [
+        {
+          name: 'Track 1',
+          artist: 'Artist 1',
+          album: 'Album 1',
+          id: '001',
+        },
+        {
+          name: 'Track 2',
+          artist: 'Artist 2',
+          album: 'Album 2',
+          id: '002',
+        },
+        {
+          name: 'Track 3',
+          artist: 'Artist 3',
+          album: 'Album 3',
+          id: '003',
+        }
+      ]
+    };
+  }
+
   render() {
     return (
       <div>
@@ -12,7 +38,7 @@ class App extends React.Component {
         <div className="App">
           <SearchBar />
           <div className="App-playlist">
-            <SearchResults />
+            <SearchResults searchResults={this.state.searchResults}/>
             <Playlist />
           </div>
         </div>
